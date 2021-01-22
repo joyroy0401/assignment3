47c23b9c0e07c79492bc0a4952135ba6178d3571
firstwebsite <joyroy04010@gmail.com>
2021-01-22 20:14:41 +0600

    first commit

--- assignment3.js
+++ assignment3.js
@@ -1,25 +1,34 @@
-
+// Github link- https://github.com/joyroy0401/assignment3
 
 // Start kilometerToMeter---------
 
-function kilometerToMeter(kilometer){
+function kilometerToMeter(kilometer){ 
     var meter = kilometer * 1000;
     return meter;
 }
 var result = kilometerToMeter(10);
 console.log(result);
 
-var rahim = [12, 34, 45, 60];
 
-var nanaMeter = kilometerToMeter(rahim[1]);
-console.log(nanaMeter);
+// Start budgetCalculator------------
+
+function budgetCalculator(watch, mobile, laptop){
+    var watch = watch * 50;
+    var mobile = mobile * 100;
+    var laptop = laptop *500;
+
+    var totalPrice = watch + mobile + laptop;
+    return totalPrice;
+}
+
+    var result = budgetCalculator(6, 4, 0)
+    console.log(result);
 
 
 // Start hotelCost-------
 
 function hotelCost(day){
-
-var Cost = 0;
+    var Cost = 0;
 if(day <= 10){
     cost = day * 100;
 }
@@ -43,12 +52,8 @@ else {
     console.log("Nagative value isn't allow. Thanks ");
 }
 }
-
-var result = hotelCost(-30);
-console.log(result);
-
-
-
+    var result = hotelCost(-30);
+    console.log(result);
 
 
 
@@ -65,20 +70,6 @@ function megaFriend(friend){
     }
     return largeName;
 }
-var result = megaFriend(["sujon", "shajue", "sheju", "Baju"]);
-console.log(result);
+    var result = megaFriend(["sujon", "shajue", "sheju", "Baju"]);
+    console.log(result);
 
-
-// Start budgetCalculator------------
-
-function budgetCalculator(watch, mobile, laptop){
-    var watch = watch * 50;
-    var mobile = mobile * 100;
-    var laptop = laptop *500;
-
-    var totalPrice = watch + mobile + laptop;
-    return totalPrice;
-}
-
-var result = budgetCalculator(6, 4, 0)
-console.log(result);
